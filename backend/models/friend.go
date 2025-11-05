0d2428f985403fd06b23da3f73ef6baf8a4e5d88
connor-williamson <130194797+connor-williamson@users.noreply.github.com>
2025-11-05 17:09:45 -0600

    updated DB Model

--- backend/models/friend.go
+++ backend/models/friend.go
@@ -6,14 +6,14 @@ import (
 
 // Friend represents a friendship relationship between two accounts.
 type Friend struct {
+	gorm.Model
+	
 	ID        uint       `gorm:"primaryKey"`
 	CreatedAt *time.Time `gorm:"default:null"`
 	UpdatedAt *time.Time `gorm:"default:null"`
 	DeletedAt *time.Time `gorm:"default:null;index"`
-	
 	UserID   uint `gorm:"not null"` // The user who initiated the friendship
 	FriendID uint `gorm:"not null"` // The user who is the friend
-	
 	// Optional: Reference to the Account model
 	User   Account `gorm:"foreignKey:UserID"`
 	Friend Account `gorm:"foreignKey:FriendID"`
